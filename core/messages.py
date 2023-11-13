058a53008be3b1953e7a225d5be1a1d95b9ae8ed
Pipe930 <felip.sanchezc@duocuc.cl>
2023-11-12 23:25:59 -0300

    Se creo un archivo para los mensajes de respuesta del servidor

---
+++ core/messages.py
@@ -0,0 +1,20 @@
+
+# Function message response a view
+def message_response_list(data):
+    return {"status": "OK", "data": data}
+
+def message_response_created(object:str, data):
+    return {"status": "Created", "data": data, "message": f"{object} se creo con exito"}
+
+def message_response_bad_request(object:str, data, method):
+
+    if method == "PUT":
+        return {"status": "Bad Request", "errors": data, "message": f"Error, {object} no se actualizo"}
+    elif method == "POST":
+        return {"status": "Bad Request", "errors": data, "message": f"Error, {object} no se creo"}
+
+def message_response_no_content(object:str):
+    return {"status": "No Content", "message": f"No tenemos {object}"}
+
+def message_response_update(object:str, data):
+    return {"status": "Reset Content", "data": data, "message": f"Se actualizo {object} con exito"}
